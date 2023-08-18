a5cea68c2d46333396e318d8811fc3996243e524
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-08-18 13:03:26 +0200

    Initial Commit

---
+++ NetworkManager/NetworkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerApp.swift
+//  NetworkManager
+//
+//  Created by Alexander Sobolev on 18.8.23..
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
